e613ef2b1620b123502c181fa263dc0bf6e8700e
Gogs <gogs@fake.local>
2020-02-09 13:42:33 +0700

    Some progress, haskell is hard

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+
 module Lib
     ( startApp
     , app
@@ -12,7 +13,6 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-
 data Movie = Movie
   { movieId :: Int
   , title :: String
@@ -22,7 +22,15 @@ data Movie = Movie
 
 $(deriveJSON defaultOptions ''Movie)
 
+--newtype MovieIds = MovieIds { ids :: [Int] }
+
+data MovieId = MovieId { id :: String }
+
+
 type API = "movies" :> Get '[JSON] [Movie]
+
+type API2 = "movies2" :> Get '[JSON] [Movie]
+  :<|> "moviestest" :> QueryParam "id" String :> Get '[JSON] MovieId
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -36,11 +44,21 @@ api = Proxy
 server :: Server API
 server = return movies
 
+server2 :: Server API2
+server2 = movies2
+    :<|> moviestest
+
+    where movies2 = return movies
+
+          moviestest :: Maybe String -> Handler MovieId
+          moviestest id = return . MovieId $ case id of
+            Nothing -> "Hello no id given coward"
+            Just n -> "nice id you got there : "
+
 movies :: [Movie]
 movies = [ Movie 1 "title1" "genres1" ["tag11", "tag12"]
          , Movie 2 "title2" "genres2" ["tag21", "tag22"]
          ]
-
 
 --                 .route(GET(URL_PREFIX + "/movie/{movieId}"), handler::getMovie)
    --                .andRoute(GET(URL_PREFIX + "/movies"), handler::getMovies)
