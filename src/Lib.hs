8db4267c2e09a0fe2affdb858e8d7320de107b07
Gogs <gogs@fake.local>
2020-02-10 16:36:40 +0700

    Movie needed a constructor with a String to work

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds       #-} -- for [Json] and the String like the url
 {-# LANGUAGE TemplateHaskell #-} -- for the deriveJson method
 {-# LANGUAGE TypeOperators   #-} -- for the :>
+--{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
     ( startApp
@@ -18,13 +19,14 @@ data Movie = Movie
   , title :: String
   , genres :: String
   , tags :: [String]
-  } deriving (Eq, Show)
+  }
 
 $(deriveJSON defaultOptions ''Movie)
 
-
-startApp :: IO ()
-startApp = run 8080 app
+movies1 :: [Movie]
+movies1 = [ Movie 1 "title1" "genres1" ["tag11", "tag12"]
+         , Movie 2 "title2" "genres2" ["tag21", "tag22"]
+         ]
 
 
 -- Test
@@ -36,31 +38,60 @@ testApi = Proxy
 testServer :: Server TestMovieAPI
 testServer = return movies1
 
+--app :: Application
+--app = serve testApi testServer
+
+
+-- Position
+type API = "test" :> Get '[JSON] [Movie]
+         :<|> "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+
+data Position = Position
+  { xCoord :: Int
+  , yCoord :: Int
+  }
+$(deriveJSON defaultOptions ''Position)
+
+server3 :: Server API
+server3 = return movies1
+     :<|> position
+  where position :: Int -> Int -> Handler Position
+        position x y = return (Position x y)
+
+proxy :: Proxy API
+proxy = Proxy
+
 app :: Application
-app = serve testApi testServer
+app = serve proxy server3
+
+
 
 -- Single Movie fetching endpoint
 type SingleMovieAPI = "movie" :> Capture "id" String :> Get '[JSON] Movie
 
-singleMovieAPI :: Proxy SingleMovieAPI
-singleMovieAPI = Proxy
+singleMovieApi :: Proxy SingleMovieAPI
+singleMovieApi = Proxy
+
 
 --singleMovieServer :: Server SingleMovieAPI
 --singleMovieServer = return singleMovie
+--  where singleMovie :: String -> Movie
+--        singleMovie id = return (Movie 1 "title5" "genres1" ["tag11", "tag12"])
+
+
+--app :: Application
+--app = serve singleMovieApi singleMovieServer
 
 -- Multiple Movies fetching endpoint
 type MultipleMoviesAPI = "movies" :> QueryParams "id" [String] :> Get '[JSON] [Movie] --or return a Stream
 
 
-singleMovie :: Movie
-singleMovie = Movie 1 "title1" "genres1" ["tag11", "tag12"]
+startApp :: IO ()
+startApp = run 8080 app
 
-movies1 :: [Movie]
-movies1 = [ Movie 1 "title1" "genres1" ["tag11", "tag12"]
-         , Movie 2 "title2" "genres2" ["tag21", "tag22"]
-         ]
 
 --                 .route(GET(URL_PREFIX + "/movie/{movieId}"), handler::getMovie)
    --                .andRoute(GET(URL_PREFIX + "/movies"), handler::getMovies)
    --                .andRoute(GET(URL_PREFIX + "/cache/refresh"), handler::refreshCache)
    --                .andRoute(GET("/readiness"), handler::isCacheReady);
+
